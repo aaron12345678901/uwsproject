c834c453ace8af06f82503e7da83dc6dec812b71
Ubuntu <ubuntu@ip-172-31-94-64.ec2.internal>
2024-10-16 14:37:49 +0000

    Initial commit of Laravel project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>recipe site</title>
+</head>
+<body>
+
+<div class="header-main"><h1>fsgfsdgdf</h1>
+    <div class="logo">
+
+    </div>
+    <div class="search-bar">
+
+    </div>
+
+</div>
+
+
+
+
+
+</body>
+</html>
---
+++ resources/views/post.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<title>recipe site</title>
+<link rel="stylesheet" href="/app.css">
+
+
+  <body>
+<div class="main">
+
+
+
+  
+  <h1><a href="/post"> first</a> </h1>
+
+
+   <article>
+    <?= $post; ?>
+  </article>
+ 
+</div>
+<a href="/">go back</a>
+
+
+
+  </body>
